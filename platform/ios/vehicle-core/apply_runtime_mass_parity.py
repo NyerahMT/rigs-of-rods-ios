07b5337eee3dba75bca05b9c678078798381c9f0
Matthew Talafous <68287092+NyerahMT@users.noreply.github.com>
2026-09-15 13:18:19 -0400

    iOS: chain per-node tyre friction parity

--- platform/ios/vehicle-core/apply_runtime_mass_parity.py
+++ platform/ios/vehicle-core/apply_runtime_mass_parity.py
@@ -11,4 +11,5 @@ if not args:
 subprocess.check_call([sys.executable, str(here / "apply_runtime_mass_parity_base.py"), *args])
 subprocess.check_call([sys.executable, str(here / "apply_wheel_parity.py"), *args])
 subprocess.check_call([sys.executable, str(here / "apply_wheel_format_parity.py"), *args])
+subprocess.check_call([sys.executable, str(here / "apply_node_friction_parity.py"), *args])
 subprocess.check_call([sys.executable, str(here / "apply_meshwheel_build_bridge.py"), *args])
