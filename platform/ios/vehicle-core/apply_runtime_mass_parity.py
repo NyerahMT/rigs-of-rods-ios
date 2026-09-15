3a35f0c6945cc2f6f8eff7db34d2ab4d82121f29
Matthew Talafous <68287092+NyerahMT@users.noreply.github.com>
2026-09-15 13:50:28 -0400

    iOS: chain Simple2 surface parity

--- platform/ios/vehicle-core/apply_runtime_mass_parity.py
+++ platform/ios/vehicle-core/apply_runtime_mass_parity.py
@@ -12,6 +12,7 @@ subprocess.check_call([sys.executable, str(here / "apply_runtime_mass_parity_bas
 subprocess.check_call([sys.executable, str(here / "apply_wheel_parity.py"), *args])
 subprocess.check_call([sys.executable, str(here / "apply_wheel_format_parity.py"), *args])
 subprocess.check_call([sys.executable, str(here / "apply_node_friction_parity.py"), *args])
+subprocess.check_call([sys.executable, str(here / "apply_simple2_ground_model_parity.py"), *args])
 subprocess.check_call([sys.executable, str(here / "apply_wheel_brake_parity.py"), *args])
 subprocess.check_call([sys.executable, str(here / "apply_default_diff_parity.py"), *args])
 subprocess.check_call([sys.executable, str(here / "apply_meshwheel_build_bridge.py"), *args])
