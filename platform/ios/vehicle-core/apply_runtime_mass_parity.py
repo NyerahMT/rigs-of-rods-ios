b43513d567b2c73b5078172abc6510f05f9e1bac
Matthew Talafous <68287092+NyerahMT@users.noreply.github.com>
2026-09-16 11:02:39 -0400

    iOS parity 5/14: enable advanced shock force transform

--- platform/ios/vehicle-core/apply_runtime_mass_parity.py
+++ platform/ios/vehicle-core/apply_runtime_mass_parity.py
@@ -14,6 +14,7 @@ subprocess.check_call([sys.executable, str(here / "apply_wheel_parity.py"), *arg
 subprocess.check_call([sys.executable, str(here / "apply_wheel_format_parity.py"), *args])
 subprocess.check_call([sys.executable, str(here / "apply_node_friction_parity.py"), *args])
 subprocess.check_call([sys.executable, str(here / "apply_shock_parity.py"), *args])
+subprocess.check_call([sys.executable, str(here / "apply_shock23_force_parity.py"), *args])
 subprocess.check_call([sys.executable, str(here / "apply_force_order_parity.py"), *args])
 subprocess.check_call([sys.executable, str(here / "apply_ground_contact_node_parity.py"), *args])
 subprocess.check_call([sys.executable, str(here / "apply_simple2_ground_model_parity.py"), *args])
